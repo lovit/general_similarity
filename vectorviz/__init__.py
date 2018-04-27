89a6ab74a3b7af6d1f97960ced70c8388d4f8b7a
lovit <soy.lovit@gmail.com>
2018-04-27 20:12:01 +0900

    Move data generator to soydata repository

--- vectorviz/__init__.py
+++ vectorviz/__init__.py
@@ -1,12 +1,8 @@
 __author__ = 'lovit'
 
-from . import data
 from . import embedding
 from . import metric
-from . import visualize
 
-__all__ = ['data',
-           'embedding',
-           'metric',
-           'visualize'
+__all__ = ['embedding',
+           'metric'
           ]
--- vectorviz/visualize/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._plot import ipython_3d_scatter
-from ._plot import ipython_2d_scatter
