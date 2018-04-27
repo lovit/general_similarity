a2b930d4d3f52acb22ec3c0046899cc53fa273ac
lovit <soy.lovit@gmail.com>
2018-04-27 16:09:22 +0900

    Commit 2d scatter plot

--- vectorviz/visualize/_plot.py
+++ vectorviz/visualize/_plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 from plotly.offline import plot, iplot
 import plotly.graph_objs as go
 from plotly.offline import init_notebook_mode
@@ -41,4 +42,49 @@ def ipython_3d_scatter(X, color, text=None,
     )
 
     fig = go.Figure(data=[data], layout=layout)
+    iplot(fig)
+
+def ipython_2d_scatter(X, color, text=None,
+        width=600, height=600, marker_size=3, colorscale='Jet'):
+
+    colorset = np.unique(color)
+    if colorset.shape[0] == 1:
+        colormap = np.asarray([0])
+    else:
+        colormap = (colorset - colorset[0]) / (colorset[-1] - colorset[0])
+
+    text = text if text else ['point #{}'.format(i) for i in range(X.shape[0])]
+
+    data = []
+    for label in colorset:
+        indices = np.where(color == label)[0]
+        Xsub = X[indices]
+        trace = go.Scatter(
+            x = Xsub[:,0],
+            y = Xsub[:,1],
+            text = [text[i] for i in indices],
+            mode = 'markers',
+            marker = dict(
+                size=marker_size,
+                color=colormap[label],
+                colorscale=colorscale
+            ),
+            opacity=0.8
+        )
+        data.append(trace)
+    
+    layout = go.Layout(
+        autosize=False,
+        width=width,
+        height=height,
+        margin=go.Margin(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad=4
+        )
+    )
+
+    fig = go.Figure(data=data, layout=layout)
     iplot(fig)
